d18f4f0e034c06e8befbb62ab6f6cfaa4cdd57ee
heptaliane <heptaliane@gmail.com>
2017-05-03 17:42:20 +0900

    add slitherlink image builder

--- simple/localFiles.cpp
+++ simple/localFiles.cpp
@@ -105,3 +105,141 @@ void LocalFileDealer::readHeader() {
     data.pop_back();
 }
 
+
+const cv::Vec3b ImageBuilder::foreColor = cv::Vec3b(0x00, 0x00, 0x00);
+
+const cv::Vec3b ImageBuilder::backColor = cv::Vec3b(0xff, 0xff, 0xff);
+
+ImageBuilder::ImageBuilder(const std::string &path) {
+
+    char c, width, height;
+    int i, j, k;
+    std::ifstream fin(path, std::ios::binary);
+
+    if (!fin) {
+        std::cout << "data set not found" << std::endl;
+    }
+
+    for (k = 0; k < 18; ++k) {
+        fin.read(&height, sizeof(height));
+        fin.read(&width, sizeof(width));
+        data[k].resize(height, width);
+
+        for (i = 0; i < height; ++i) {
+            for (j = 0; j < width; ++j) {
+                fin.read(&c, sizeof(c));
+                data[k].set(i, j, c);
+            }
+        }
+    }
+
+    fin.close();
+}
+
+void ImageBuilder::write(
+        const std::string &path, const Matrix &cell, const Matrix &rgrid,
+        const Matrix &cgrid, const Matrix &vertex) const {
+
+    int width = data[0].cols() * cell.cols() + data[11].cols() * vertex.cols();
+    int height = data[0].rows() * cell.rows()
+        + data[11].rows() * vertex.rows();
+    cv::Mat image(cv::Size(width, height), 16);
+
+    int i, j;
+    for (i = 0; i < cell.rows(); ++i) {
+        for (j = 0; j < cell.cols(); ++j) {
+            drawCell(i, j, cell.get(i, j), &image);
+        }
+    }
+    for (i = 0; i < rgrid.rows(); ++i) {
+        for (j = 0; j < rgrid.cols(); ++j) {
+            drawRow(i, j, rgrid.get(i, j), &image);
+        }
+    }
+    for (i = 0; i < cgrid.rows(); ++i) {
+        for (j = 0; j < cgrid.cols(); ++j) {
+            drawCol(i, j, cgrid.get(i, j), &image);
+        }
+    }
+    for (i = 0; i < vertex.rows(); ++i) {
+        for (j = 0; j < vertex.cols(); ++j) {
+            drawVertex(i, j, vertex.get(i, j), &image);
+        }
+    }
+
+    cv::imwrite(path, image);
+}
+
+void ImageBuilder::drawCell(
+        int row, int col, int status, cv::Mat* image) const {
+
+    if (status < 0 || status > 4) {
+        status = 4;
+    }
+
+    int rbegin = data[0].rows() * row + data[5].rows() * (row + 1);
+    int cbegin = data[0].cols() * col + data[8].cols() * (col + 1);
+
+    setImagePixel(rbegin, cbegin, data[status], image);
+}
+
+void ImageBuilder::drawRow(
+        int row, int col, int status, cv::Mat* image) const {
+
+    if (status < 0 || status > 2) {
+        status = 2;
+    }
+
+    int rbegin = data[0].rows() * row + data[5].rows() * row;
+    int cbegin = data[5].cols() * col + data[11].cols() * (col + 1);
+
+    setImagePixel(rbegin, cbegin, data[status + 5], image);
+}
+
+void ImageBuilder::drawCol(
+        int row, int col, int status, cv::Mat* image) const {
+
+    if (status < 0 || status > 2) {
+        status = 2;
+    }
+
+    int rbegin = data[8].rows() * row + data[11].rows() * (row + 1);
+    int cbegin = data[0].cols() * col + data[8].cols() * col;
+
+    setImagePixel(rbegin, cbegin, data[status + 8], image);
+}
+
+void ImageBuilder::drawVertex(
+        int row, int col, int status, cv::Mat* image) const {
+
+    if (status < 0 || status > 2) {
+        status = 2;
+    }
+
+    int rbegin = data[11].rows() * row + data[8].rows() * row;
+    int cbegin = data[11].cols() * col + data[5].cols() * col;
+
+    setImagePixel(rbegin, cbegin, data[status + 11], image);
+}
+
+void ImageBuilder::setImagePixel(
+        int rbegin, int cbegin, const Matrix &mat, cv::Mat* image) const {
+
+    int i, j, row = 0, col = 0;
+    cv::Vec3b* ptr;
+
+    for (i = 0; i < mat.rows(); ++i) {
+        row = rbegin + i;
+        ptr = image->ptr<cv::Vec3b>(row);
+
+        for (j = 0; j < mat.cols(); ++j) {
+            col = cbegin + j;
+
+            if (mat.get(i, j) == 1) {
+                ptr[col] = foreColor;
+            } else {
+                ptr[col] = backColor;
+            }
+        }
+    }
+}
--- simple/localFiles.h
+++ simple/localFiles.h
@@ -1,9 +1,12 @@
 #ifndef LOCAL_FILES_H_
 #define LOCAL_FILES_H_
 
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
 #include "slarray.h"
 
 class LocalFileDealer {
@@ -42,6 +45,38 @@ private:
     int height;
     std::vector<char> header;
     std::vector<char> data;
+};
+
+class ImageBuilder {
+public:
+
+    explicit ImageBuilder(const std::string &path);
+
+    void write(
+            const std::string &path, const Matrix &cell,
+            const Matrix &rgrid, const Matrix &cgrid,
+            const Matrix &vertex) const;
+private:
+
+    void drawCell(int row, int col, int status, cv::Mat* image) const;
+
+    void drawRow(int row, int col, int status, cv::Mat* image) const;
+
+    void drawCol(int row, int col, int status, cv::Mat* image) const;
+
+    void drawVertex(int row, int col, int status, cv::Mat* image) const;
+
+    void setImagePixel(
+            int rbegin, int cbegin, const Matrix &mat, cv::Mat* Image) const;
+
+    static const cv::Vec3b foreColor;
+    static const cv::Vec3b backColor;
+
+    // 0 - 4 : cell
+    // 5 - 7 : row
+    // 8 - 10 : col
+    // 11 - 17 : vertex
+    Matrix data[18];
 };
 
 #endif
